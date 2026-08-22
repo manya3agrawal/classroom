01f97dde0d028a2da35850a738e3d2862a766db1
manya3agrawal <manya3agrawal@gmail.com>
2026-08-22 10:29:06 +0530

    Updated code according to question

--- src/probelms/getter_setter.java
+++ src/probelms/getter_setter.java
@@ -1,28 +1,22 @@
 package probelms;
-class Stu{
+class Employee{
     private String name;
     private int id;
-    private int salary;
     private String department;
-    Stu(String name, int id, int salary, String department) {
+    private int salary;
+    static final String companyName = "Tech Solutions";
+    //static String companyName;
+    static int employeeCount ;
+
+    Employee(String name, int id, int salary, String department) {
         this.name = name;
         this.id = id;
         this.salary = salary;
         this.department = department;
+        employeeCount++;
     }
-    public void setname(String name){
-        this.name = name;
-    }
-    public void setid(int id){
-        this.id = id;
-    }
-    public void setsalary(int salary){
-        this.salary = salary;
-    }
-    public void setdepartment(String department){
-        this.department = department;
-    }
-    public String getname(){
+
+    public String getName(){
         return name;
     }
     public int getSalary(){
@@ -37,18 +31,12 @@ class Stu{
 }
 
 public class getter_setter {
-
     public static void main(String[] args) {
-        Stu obj = new Stu("Manya",18,4000000,"CSE AIML");
-        //set details
-        obj.setname("Manya");
-        obj.setid(1);
-        obj.setsalary(4000000);
-        obj.setdepartment("CSE AIML");
-       //get details
-        System.out.println(obj.getname());
-        System.out.println(obj.getSalary());
-        System.out.println(obj.getdepartment());
-        System.out.println(obj.getid());
+        Employee e1 = new Employee("Amit", 101, 45000, "IT");
+        Employee e2 = new Employee("Neha", 102, 55000, "HR");
+        System.out.println(e1.getName());
+        System.out.println("Salary: " + e2.getSalary());
+        System.out.println("Company: " + Employee.companyName);
+        System.out.println("Total Employees: " + Employee.employeeCount);
     }
 }
