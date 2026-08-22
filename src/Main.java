2b28921f12cc5c99f69b9d749f1bd83078f08e2c
manya3agrawal <manya3agrawal@gmail.com>
2026-08-22 09:40:17 +0530

    class problems

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,49 @@
-class Student{
+class Student {
     String name = "Manya";
     int id = 18;
-    void display(){
+
+    void display() {
         System.out.println(name);
         System.out.println(id);
     }
-}//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+}
+interface Employ {
+    void EmpInfo();
+}
+
+interface EmpSalary {
+    void SalaryInfo();
+
+}
+class St{
+    String name ;
+    int rollno;
+    public void display(String name,int rollno){
+        
+    }
+}
+class InheritanceEx implements Employ,EmpSalary {
+    String name;
+    int age;
+    double salary;
+
+    InheritanceEx(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+    @Override
+    public void EmpInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    @Override
+    public void SalaryInfo() {
+        System.out.println("Salary: " + salary);
+    }
+}
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
---
+++ src/probelms/getter_setter.java
@@ -0,0 +1,48 @@
+package probelms;
+class Stu{
+    private String name;
+    private int id;
+    private int salary;
+    private String department;
+    public void setname(String name){
+        this.name = name;
+    }
+    public void setid(int id){
+        this.id = id;
+    }
+    public void setsalary(int salary){
+        this.salary = salary;
+    }
+    public void setdepartment(String department){
+        this.department = department;
+    }
+    public String getname(){
+        return name;
+    }
+    public int getSalary(){
+        return salary;
+    }
+    public int getid(){
+        return id;
+    }
+    public String getdepartment(){
+        return department;
+    }
+}
+
+public class getter_setter {
+
+    public static void main(String[] args) {
+        Stu obj = new Stu();
+        //set details
+        obj.setname("Manya");
+        obj.setid(1);
+        obj.setsalary(4000000);
+        obj.setdepartment("CSE AIML");
+       //get details
+        System.out.println(obj.getname());
+        System.out.println(obj.getSalary());
+        System.out.println(obj.getdepartment());
+        System.out.println(obj.getid());
+    }
+}
